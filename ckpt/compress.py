331cbdddceb87ceb2f23d50a3ab4701f4c57ef05
Liang Shuhao <liangshuhao@pku.edu.cn>
2022-05-04 10:59:00 +0800

    Added support for merging compressed pages

--- ckpt/compress.py
+++ ckpt/compress.py
@@ -30,7 +30,8 @@ def decompress(input: bytes, maxout: int) -> bytes:
 parser = argparse.ArgumentParser()
 parser.add_argument('cfg')
 parser.add_argument('dump')
-parser.add_argument('-r', '--threshold', type=float, default=0.3)
+parser.add_argument('-r', '--max-ratio', type=float, default=0.3)
+parser.add_argument('-l', '--max-length', type=int, default=2048)
 
 if __name__ == '__main__':
     args = parser.parse_args()
@@ -64,48 +65,59 @@ if __name__ == '__main__':
     dumpfile.close()
 
     # try compressing data
-    uncomp, comp = [], []
-    orig_size, shrink_size = 0, 0
+    comp = []  # addr, [data, ...]
+    uncomp = []  # addr, [data, ...]
+    comp_pages = 0
     for addr, data in sorted(page_map.items()):
-        orig_size += len(data)
         comp_data = compress(data)
-        if len(comp_data) <= len(data) * args.threshold:
-            comp.append((addr, comp_data))
-            shrink_size += len(data) - len(comp_data)
+        # compressed pages may be coalesced if each page fits
+        # into the min compression ratio and the total length
+        # is within the max length
+        if len(comp_data) <= len(data) * args.max_ratio:
+            if not comp:
+                comp.append((addr, [data]))
+            elif comp[-1][0] + len(comp[-1][1]) * 4096 < addr:
+                comp.append((addr, [data]))
+            elif len(compress(b''.join(comp[-1][1]) + data)) > args.max_length:
+                comp.append((addr, [data]))
+            else:
+                comp[-1][1].append(data)
+            comp_pages += 1
+        # uncompressed pages can be freely coalesced
         else:
-            uncomp.append((addr, data))
-    print('compressed pages: %d/%d' % (len(comp), len(page_map)))
-    print('compression ratio: %.1f%%' % (shrink_size / orig_size * 100))
+            if not uncomp:
+                uncomp.append((addr, [data]))
+            elif uncomp[-1][0] + len(uncomp[-1][1]) * 4096 < addr:
+                uncomp.append((addr, [data]))
+            else:
+                uncomp[-1][1].append(data)
 
     # dump processed image
     cfgfile = open('.c'.join(os.path.splitext(args.cfg)), 'w')
     dumpfile = open('.c'.join(os.path.splitext(args.dump)), 'wb')
-    cfgs = []  # addr, offset, size, length
     offset = 0
+    cfgfile.write('%d\n' % (len(comp) + len(uncomp)))
 
     # dump uncompressed pages first since they need to be
-    # page-aligned. Uncompressed pages may be coalesced
+    # page-aligned
     for addr, data in uncomp:
-        if cfgs and cfgs[-1][0] + cfgs[-1][2] == addr:
-            cfgs[-1][2] += 4096
-            cfgs[-1][3] = cfgs[-1][2]
-        else:
-            cfgs.append([addr, offset, 4096, 4096])
-        offset += 4096
-        dumpfile.write(data)
+        size = len(data) * 4096
+        cfgfile.write('%x\t%x\t%x\t%x\n' % (addr, offset, size, size))
+        dumpfile.write(b''.join(data))
+        offset += size
 
-    # compressed pages are currently seperated. This is due
-    # to the limited size of the decompressor buffer
     for addr, data in comp:
-        cfgs.append([addr, offset, 4096, len(data)])
-        dumpfile.write(data)
-        offset += len(data)
+        size = len(data) * 4096
+        comp_data = compress(b''.join(data))
+        length = len(comp_data)
+        cfgfile.write('%x\t%x\t%x\t%x\n' % (addr, offset, size, length))
+        dumpfile.write(comp_data)
+        offset += length
 
-    cfgfile.write('%d\n' % len(cfgs))
-    for addr, offset, size, length in cfgs:
-        cfgfile.write('%x\t%x\t%x\t%x\n' % (
-            addr, offset, size, length))
     cfgfile.write('%x\n' % entry_pc)
-
     cfgfile.close()
     dumpfile.close()
+
+    print('compressed pages: %d/%d' % (comp_pages, len(page_map)))
+    comp_ratio = 1 - offset / (len(page_map) * 4096)
+    print('compression ratio: %.1f%%' % (comp_ratio * 100))
